d0d490684967c45d09b305e488a62480f5df43ed
aeneasr <aeneas@ory.sh>
2019-05-18 13:12:05 +0200

    u

---
+++ .circleci/release_name.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+$release=$(curl -s 'https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases')
+$tag=jq -r '.[0].tag_name'
+$tag_name=jq -r '.[0].name'
+
+if [ -n "$tag_name" ]; then
+    echo "export RELEASE_NAME=$tag_name"
+    exit 0
+fi
+
+echo "export RELEASE_NAME=$tag"
